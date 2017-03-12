92b4099de3ade85c798b541278d4a5eb80636b04
Konstantinos Skenteridis <kostaking@gmail.com>
2017-03-13 01:30:54 +0200

    Add files via upload

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int max_Health = 10;
+	//public int max_Health = 100;
+	public int cur_Health = 0;
+	//public int cur_Health = 0;
+	public GameObject healthBar;
+	public GameObject playerObject;
+	public int myvalue,calc_Health;
+	//public Text valueText;
+	// Use this for initialization
+	void Start () {
+	
+	
+		cur_Health = max_Health;
+		//string[] tmp = valueText.text.Split (':');
+		//valueText.text = tmp [0] + ": " + cur_Health;
+		//InvokeRepeating("decreasehealth",1f,1f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Debug.Log (cur_Health);
+	
+	}
+
+
+	public void decreasehealth(int theValue)
+	{
+
+		//myvalue = theValue;
+		//cur_Health -= 2f;
+		//float calc_Health = cur_Health / max_Health;
+		calc_Health = cur_Health - theValue;
+		cur_Health = calc_Health;
+		Debug.Log (cur_Health);
+		//SetHealthBar (calc_Health);
+		SetHealthBar (calc_Health);
+		if (cur_Health <= 0) {
+
+			DestroyObject (playerObject);
+
+			//CancelInvoke ();
+		}
+
+	}
+
+	public void SetHealthBar(int myHealth)
+	{
+
+		healthBar.transform.localScale = new Vector3 (myHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+	}
+}
